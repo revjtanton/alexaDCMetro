be83908b78038b9a287e37754df9a385aa232a6a
antonj <jason_anton@sra.com>
2015-10-16 22:33:26 -0400

    working version with bad vars

--- index.js
+++ index.js
@@ -9,19 +9,13 @@
 */
 
 /**
- * This simple sample has no external dependencies or session management, and shows the most basic
- * example of how to create a Lambda function for handling Alexa Skill requests.
- *
- * Examples:
- * One-shot model:
- *  User: "Alexa, tell Greeter to say hello"
- *  Alexa: "Hello World!"
+ * This is an Alexa Skill for reporting the current DC Metro status for each line.
  */
 
 /**
  * App ID for the skill
  */
-var APP_ID = 'amzn1.echo-sdk-ams.app.d7aa8cbb-2468-4863-96be-0ce1b9cbbd1a'; //replace with "amzn1.echo-sdk-ams.app.[your-unique-value-here]";
+var APP_ID = 'amzn1.echo-sdk-ams.app.3d2e1dd8-1405-4c01-aa24-f75acc1020d4'; //replace with "amzn1.echo-sdk-ams.app.[your-unique-value-here]";
 
 /**
  * The AlexaSkill prototype and helper functions
@@ -29,63 +23,67 @@ var APP_ID = 'amzn1.echo-sdk-ams.app.d7aa8cbb-2468-4863-96be-0ce1b9cbbd1a'; //re
 var AlexaSkill = require('./AlexaSkill');
 
 /**
- * dcMetro is a child of AlexaSkill.
+ * HelloWorld is a child of AlexaSkill.
+ * To read more about inheritance in JavaScript, see the link below.
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Introduction_to_Object-Oriented_JavaScript#Inheritance
  */
-var dcMetro = function () {
+var HelloWorld = function () {
     AlexaSkill.call(this, APP_ID);
 };
 
 // Extend AlexaSkill
-dcMetro.prototype = Object.create(AlexaSkill.prototype);
-dcMetro.prototype.constructor = HelloWorld;
+HelloWorld.prototype = Object.create(AlexaSkill.prototype);
+HelloWorld.prototype.constructor = HelloWorld;
 
-dcMetro.prototype.eventHandlers.onSessionStarted = function (sessionStartedRequest, session) {
+HelloWorld.prototype.eventHandlers.onSessionStarted = function (sessionStartedRequest, session) {
     console.log("HelloWorld onSessionStarted requestId: " + sessionStartedRequest.requestId
         + ", sessionId: " + session.sessionId);
     // any initialization logic goes here
 };
 
-dcMetro.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
-    console.log("dcMetro onLaunch requestId: " + launchRequest.requestId + ", sessionId: " + session.sessionId);
-    var speechOutput = "Thank you for inquiring about the DC Metro status.  What line where you curious about?  You can say Red, Orange, Silver, Blue, Yellow, or Green to get the status of a particular line.";
-    var repromptText = "You inquired about the status of the D.C. Metro.  You can say Red, Orange, Silver, Blue, Yellow, or Green to get the status of a particular line.";
+HelloWorld.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
+    console.log("HelloWorld onLaunch requestId: " + launchRequest.requestId + ", sessionId: " + session.sessionId);
+    var speechOutput = "test";
+    var repromptText = "test";
     response.ask(speechOutput, repromptText);
 };
 
-dcMetro.prototype.eventHandlers.onSessionEnded = function (sessionEndedRequest, session) {
+HelloWorld.prototype.eventHandlers.onSessionEnded = function (sessionEndedRequest, session) {
     console.log("HelloWorld onSessionEnded requestId: " + sessionEndedRequest.requestId
         + ", sessionId: " + session.sessionId);
     // any cleanup logic goes here
 };
 
-dcMetro.prototype.intentHandlers = {
+HelloWorld.prototype.intentHandlers = {
     // register custom intent handlers
     RedLineIntent: function (intent, session, response) {
-        response.tellWithCard("Placeholder for red status", "DC Metro Status", "Placeholder for red status");
+        response.tellWithCard("red line test", "Greeter", "red line test");
     },
     OrangeLineIntent: function (intent, session, response) {
-        response.tellWithCard("Placeholder for orange status", "DC Metro Status", "Placeholder for orange status");
+        response.tellWithCard("orange line test", "Greeter", "orange line test");
     },
     SilverLineIntent: function (intent, session, response) {
-        response.tellWithCard("Placeholder for silver status", "DC Metro Status", "Placeholder for silver status");
+        response.tellWithCard("silver line test", "Greeter", "silver line test");
     },
     BlueLineIntent: function (intent, session, response) {
-        response.tellWithCard("Placeholder for blue status", "DC Metro Status", "Placeholder for blue status");
+        response.tellWithCard("blue line test", "Greeter", "blue line test");
     },
     YellowLineIntent: function (intent, session, response) {
-        response.tellWithCard("Placeholder for yellow status", "DC Metro Status", "Placeholder for yellow status");
+        response.tellWithCard("yellow line test", "Greeter", "yellow line test");
     },
     GreenLineIntent: function (intent, session, response) {
-        response.tellWithCard("Placeholder for green status", "DC Metro Status", "Placeholder for green status");
+        response.tellWithCard("green line test", "Greeter", "green line test");
     },
     HelpIntent: function (intent, session, response) {
-        response.ask("You can say Red, Orange, Silver, Blue, Yellow, or Green to get the status of a particular line.");
+        response.ask("help test.", "help test.");
     }
 };
 
 // Create the handler that responds to the Alexa Request.
 exports.handler = function (event, context) {
-    var dcMetro = new dcMetro();
-    dcMetro.execute(event, context);
+    // Create an instance of the HelloWorld skill.
+    var helloWorld = new HelloWorld();
+    helloWorld.execute(event, context);
 };
 
